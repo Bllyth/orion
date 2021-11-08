390293782148e4b338b075ccf9463f391a933fa3
Bllyth <muriithi.leonard1@students.ku.ac.ke>
2021-11-08 09:22:50 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, request, current_app, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 from flask_babel import Babel
-from flask_login import LoginManager
+# from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy()
-migrate = Migrate()
-login = LoginManager()
+#db = SQLAlchemy()
+#migrate = Migrate()
+#login = LoginManager()
 babel = Babel()
 
 
@@ -16,10 +16,10 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login.init_app(app)
-    babel.init_app(app)
+   # db.init_app(app)
+#     migrate.init_app(app, db)
+#     login.init_app(app)
+#     babel.init_app(app)
 
     from app.extensions import jinja as jinja_bp
     app.register_blueprint(jinja_bp)
@@ -29,12 +29,12 @@ def create_app(config_class=Config):
 
     # from app.errors import errors as errors_bp
     # app.register_blueprint(errors_bp)
-    #
-    from app.admin import admin as admin_bp
-    app.register_blueprint(admin_bp, url_prefix='/admin')
+#     #
+#     from app.admin import admin as admin_bp
+#     app.register_blueprint(admin_bp, url_prefix='/admin')
 
-    from app.auth import auth as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+#     from app.auth import auth as auth_bp
+#     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
 
