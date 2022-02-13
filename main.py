d57158bd14278ad347c62553db84c71bf7c552b2
Bllyth <roathdoug@gmail.com>
2022-02-13 21:58:16 +0300

    Fixes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# if __name__ == '__main__':
+#     app.run()
